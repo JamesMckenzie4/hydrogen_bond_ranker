4c721d5f02969476086fc8257f5ee393e045ab74
James McKenzie <jamesmckenzie4@gmail.com>
2016-12-09 14:19:14 +0000

    changed plot linits in plot_tracked_skills()

--- scripts/plotting_functions.py
+++ scripts/plotting_functions.py
@@ -1,9 +1,7 @@
 from path_definition import scripts_dir
 
-def plot_tracked_ratings(list_of_fg, elo_tracking_dict, glicko_tracking_dict, skill_tracking_dict, name, key_location):
-    """function to track the ratings of a functional group. Function takes a list of functional groups for which the trackers
-    are to be plotted for, the elo, glicko and trueskill tracking dictionaries from calculate_and_track_ratings(), a name
-    for which the .png will be called, and the key location, 1 = top right"""
+def plot_tracked_ratings(list_of_fg_to_track, elo_tracking_dict, glicko_tracking_dict, skill_tracking_dict, name, key_location):
+    """Function to plot the track of the TrueSkill, Glicko and Elo ratings for a list of functional_groups (MAX = 7]"""
     
     sns.set(color_codes=True)
     plt.rcParams['xtick.labelsize'] = 15
@@ -19,8 +17,8 @@ def plot_tracked_ratings(list_of_fg, elo_tracking_dict, glicko_tracking_dict, sk
     for fg in list_of_fg:
         elo_y_vals = [float(i) for i in elo_tracking_dict[fg]]
         elo_x_vals = [ind[0] for ind in enumerate(elo_y_vals)]
-        ax1.set_xlim((-5,50))
-        ax1.set_ylim((0,2000)) 
+        ax1.set_xlim((-5,200))
+        ax1.set_ylim((0,2000))
         ax1.scatter(elo_x_vals, elo_y_vals, c=colors[num], alpha = 1.0, label = fg, edgecolor = "none")
         x1,x2 = ax1.get_xlim()
         y1,y2 = ax1.get_ylim()
@@ -36,7 +34,7 @@ def plot_tracked_ratings(list_of_fg, elo_tracking_dict, glicko_tracking_dict, sk
     for fg in list_of_fg:
         gli_y_vals = [i.mu for i in glicko_tracking_dict[fg]]
         gli_x_vals = [ind[0] for ind in enumerate(gli_y_vals)]
-        ax2.set_xlim((-5,50))
+        ax2.set_xlim((-5,200))
         ax2.set_ylim((0,2500))
         ax2.scatter(gli_x_vals, gli_y_vals, c=colors[num], alpha = 1.0, label = fg, edgecolor = "none")
         x3,x4 = ax2.get_xlim()
@@ -52,7 +50,7 @@ def plot_tracked_ratings(list_of_fg, elo_tracking_dict, glicko_tracking_dict, sk
     for fg in list_of_fg:
         skill_y_vals = [list(i)[0] for i in skill_tracking_dict[fg]]
         skill_x_vals = [ind[0] for ind in enumerate(skill_y_vals)]
-        ax3.set_xlim((-5,50))
+        ax3.set_xlim((-5,200))
         ax3.set_ylim((0,50))
         ax3.scatter(skill_x_vals, skill_y_vals, c=colors[num], alpha = 1.0, label = fg, edgecolor = "none")
         x5,x6 = ax3.get_xlim()
@@ -63,8 +61,8 @@ def plot_tracked_ratings(list_of_fg, elo_tracking_dict, glicko_tracking_dict, sk
         num += 1
         handles, labels = ax1.get_legend_handles_labels()
         ax3.legend(handles, labels, loc = key_location)
-      
-        #change directory to pictures
+        
         os.chdir(scripts_dir)
-        os.chdir("../pictures
         plt.savefig(name + ".png", bbox_inches="tight")
+        
+
