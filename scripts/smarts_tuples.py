10c8a18eb00dc3137b26c0e59acc357417f127b8
James McKenzie <jamesmckenzie4@gmail.com>
2017-01-26 09:59:44 +0000

    changed path to use the relative directory path instead of hard coded path

--- scripts/smarts_tuples.py
+++ scripts/smarts_tuples.py
@@ -1,24 +1,24 @@
-from collections import namedtuple
 import os
+from collections import namedtuple
+from path_definition import scripts_dir
 
 def get_smarts_tuples():
-    """opens smarts-patterns.txt and gets all the relevant information into a list of named tuples"""
+    """opens smarts-patterns.txt and creates a named tuple for each functional group containing all H-bonding paramater information,
+    heavy atom count etc. Returns a list of named tuples"""
     
-    #go to smarts patters directory - problems on linux machine with this
-    os.chdir(r"C:\Users\Helen\Documents\James\hydrogen_bonds\smarts_patterns")
-    #get data into named tuples
+    os.chdir(scripts_dir)
+    os.chdir("../smarts_patterns")
     named_tuple_list = []
     open_file = open("smarts-pattern.txt", "r")
-    smarts_named_tuple = namedtuple("SMARTS", 'functional_Group smarts Type alpha alpha_stdev beta beta_stdev heavy_atoms number_of_donor_sites number_of_acceptor_sites acceptor_heavy_atom donor_heavy_atom')
-    #parse file
+    smarts_named_tuple = namedtuple("SMARTS",'functional_Group smarts Type alpha alpha_stdev beta beta_stdev heavy_atoms number_of_donor_sites number_of_acceptor_sites acceptor_heavy_atom donor_heavy_atom')
     data = [i.split() for i in f.readlines()]
     for i in data:
         if "Functional_Group" in i:
             continue
         try:
-            y = smartsNamedTuple(i[0], i[1], i[2], i[3], i[4], i[5], i[6],
+            n_tup = smartsNamedTuple(i[0], i[1], i[2], i[3], i[4], i[5], i[6],
                                  i[7], i[8], i[9], i[10], i[11])
-            named_tuple_list.append(y)
+            named_tuple_list.append(n_tup)
         except IndexError:
             continue
     return named_tuple_list
